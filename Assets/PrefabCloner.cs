1d382451a37048f7f6fefb91cac6e72dd4579521
FrancoSandri <46960579@est.ort.edu.ar>
2022-05-20 14:26:45 -0300

    Repaso 3 completed

---
+++ Assets/PrefabCloner.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PrefabCloner : MonoBehaviour
+{
+    public GameObject prefab;
+    public Text inputCloneAmount;
+    int counter;
+    public Text displayClonesLeft;
+    public InputField IField;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        counter = 0;
+        displayClonesLeft.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ClonePrefab()
+    {
+        if (inputCloneAmount.text != "")
+        {
+            IField.readOnly = true;
+            int cloneAmount = int.Parse(inputCloneAmount.text);
+            if (counter < cloneAmount)
+            {
+                Instantiate(prefab);
+                counter++;
+                displayClonesLeft.text = (cloneAmount - counter).ToString();
+            }
+            else
+            {
+                displayClonesLeft.text = "Ya clonó suficientes objetos";
+            }
+        }else 
+        {
+            displayClonesLeft.text = "Error. Ingrese un valor";
+        }
+    }
+}
